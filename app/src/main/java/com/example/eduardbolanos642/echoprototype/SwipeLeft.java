0c43c747a82a608bd05f1fb9e56c61beae36d3ce
EduardBolanos <eduard.bolanos642@myci.csuci.edu>
2019-03-07 20:47:12 -0800

    codin it up

---
+++ app/src/main/java/com/example/eduardbolanos642/echoprototype/SwipeLeft.java
@@ -0,0 +1,13 @@
+package com.example.eduardbolanos642.echoprototype;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SwipeLeft extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstancesState){
+        super.onCreate(savedInstancesState);
+        setContentView(R.layout.activity_swipe_left);
+    }
+}
