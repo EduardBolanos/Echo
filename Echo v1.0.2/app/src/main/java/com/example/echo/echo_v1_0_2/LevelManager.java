0e8806c53f5e4549fad47af448d4f95f842e5578
Bolanos <eduard.bolanos642@SIE-1119-08.local>
2019-04-10 10:46:47 -0700

    xml, build and gradle fixes

---
+++ Echo v1.0.2/app/src/main/java/com/example/echo/echo_v1_0_2/LevelManager.java
@@ -0,0 +1,42 @@
+package com.example.echo.echo_v1_0_2;
+
+import java.util.ArrayList;
+
+public class LevelManager {
+
+    private ArrayList<Level> mLevels;
+    private int mCurrentLevel;
+    private int mAmbientSFXVolume;
+    private int mFootstepSFXVolume;
+
+    public ArrayList<Level> getLevels() {
+        return mLevels;
+    }
+
+    public void setLevels(ArrayList<Level> levels) {
+        mLevels = levels;
+    }
+
+    public void setCurrentLevel(int id)
+    {
+        mCurrentLevel = id;
+    }
+
+    public int getCurrentLevel()
+    {
+        return mCurrentLevel;
+    }
+
+    public void addLevel(Level level)
+    {
+        mLevels.add(level);
+    }
+
+    public void initializeLevel()
+    {
+        // contains important initialization logic, including item spawning, player spawn configuration,
+        // map configuration, etc
+    }
+
+
+}
